6e4b2cf538df7067b05e564ab61b446634c809e2
Conner <connervercellino@gmail.com>
2019-12-03 18:23:54 -0800

    Expose user email

--- app/src/main/java/edu/ucsb/cs/cs184/bdarnelluclickerstudent/MainActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/bdarnelluclickerstudent/MainActivity.kt
@@ -1,13 +1,18 @@
 package edu.ucsb.cs.cs184.bdarnelluclickerstudent
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    public lateinit var email: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,20 @@ class MainActivity : AppCompatActivity() {
         sign_in_button.setOnClickListener {
             val signInIntent = mGoogleSignInClient.signInIntent
             startActivityForResult(signInIntent, 1)
-
         }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInRequest(task)
+        }
+    }
+
+    private fun handleSignInRequest( completedTask: Task<GoogleSignInAccount>) {
+       var account = completedTask.result
+       email = account?.email!!
     }
 }
